bd979b8204cf4aa4835acd44e96196f05548a42e
DiegoAlcivar <dil.alcivar@yavirac.edu.ec>
2023-03-24 15:27:03 -0500

    cambios footer, centrado de contenido y arreglo icono

--- customer/includes/footer.php
+++ customer/includes/footer.php
@@ -37,6 +37,11 @@
                 <div class="phone">
                     Telefono:
                     <a class="phone__number" href="tel:0123456789">0989138892</a>
+                    <a href="https://www.facebook.com/accesworldtravel" class="banner-social__link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                    </svg>
+                </a> 
                 </div>
                 <div class="email">
                     Email:
@@ -54,10 +59,13 @@
                 <span>INK</span><br> PORTECH 2023
             </div>
 
+
             <div class="banner-social">
                 <a href="https://www.facebook.com/accesworldtravel" class="banner-social__link">
-                    <i class="icon-facebook"></i>
-                </a>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                    </svg>
+                </a>        
                 <a href="https://www.instagram.com/accesworldtravel.s.a/" class="banner-social__link">
                     <i class='icon-instagram'></i>
                 </a>
